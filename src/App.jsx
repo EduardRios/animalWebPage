12e8fbdbe0b9a6bf2cbeb1be294829924c63fccb
EdRios1608 <89537754+EdRios1608@users.noreply.github.com>
2025-06-07 17:24:24 -0500

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import AnimalCard from "./components/AnimalCard";
-import Home from "./pages/Home";
 import "./css/App.css";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No favorites yet! Add and come back!</h2>
+    </div>
+  );
+}
+
+export default Favorites;
